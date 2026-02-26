e69c6c975fb7bc79bae2acbfa317fa107f9a570a
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-02-26 13:51:57 +0530

    feat: add client code

--- patterns/creational/singleton/src/main/java/com/designpatterns/singleton/SingletonApplication.java
+++ patterns/creational/singleton/src/main/java/com/designpatterns/singleton/SingletonApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SingletonApplication {
   public static void main(String[] args) {
-    SpringApplication.run(SingletonApplication.class, args);
+    // SpringApplication.run(SingletonApplication.class, args);
+    DoubleCheckedLockingSingleton d1 = DoubleCheckedLockingSingleton.getInstance();
+    DoubleCheckedLockingSingleton d2 = DoubleCheckedLockingSingleton.getInstance();
+
+    System.out.println(d1.hashCode());
+    System.out.println(d2.hashCode());
+    System.out.println(d1 == d2);
   }
 }
